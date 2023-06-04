e94d25c582dea8a4c095dd4327b801ad7710639b
Veisy <knutveyshamsun@gmail.com>
2023-06-04 17:32:08 +0300

    Small update.

--- hb.py
+++ hb.py
@@ -93,10 +93,10 @@ def transducer_gain(S, Gams, Gaml):
     return GtdB
 
 
-S11 = phasor_to_rectangular(0.4918, 108.6)
-S12 = phasor_to_rectangular(0.128, 18.5)
-S21 = phasor_to_rectangular(3.565, 20.9)
-S22 = phasor_to_rectangular(0.548, 113.6)
+S11 = phasor_to_rectangular(0.5105, 105.7)
+S12 = phasor_to_rectangular(0.1314, 19.9)
+S21 = phasor_to_rectangular(3.454, 19.3)
+S22 = phasor_to_rectangular(0.0930, 107.5)
 print("S Parameters S11:", S11, ", S12:", S12, ", S21:", S21, ", S22:", S22)
 print("S22 conjugate: ", conj(S22))
 
